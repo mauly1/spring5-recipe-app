ad78cd9d74cc7d206166d071c9662b43f9d77f01
sunil kumar maurya <sunilkumar.maurya@team.telstra.com>
2021-09-19 19:14:55 +0530

    add domain commands and convertors for different pojo

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -18,6 +18,15 @@ public class Ingredient {
     public Ingredient() {
 
     }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure) {
         this.description = description;
         this.amount = amount;
